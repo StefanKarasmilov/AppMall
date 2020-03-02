6fecedfbe404653b32e2c58c4295aa896dd165e8
RojoF <rojof83@gmail.com>
2020-03-02 17:18:44 +0100

    Commit de prueba 2

--- app/src/main/java/com/proyecto/appmall/MainActivity.java
+++ app/src/main/java/com/proyecto/appmall/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Accion", null).show();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
