f1bd1f53498f1c8df31457d31ff228289eff2ed1
Stefan <stefankarasmilovv@gmail.com>
2020-05-27 19:55:23 +0200

    Primera versión funcional

--- app/src/main/java/com/proyecto/appmall/common/Constantes.java
+++ app/src/main/java/com/proyecto/appmall/common/Constantes.java
@@ -2,7 +2,7 @@ package com.proyecto.appmall.common;
 
 public class Constantes {
 
-    public static final String[] ADMIN_UID = {"ImJjP2kUunTy9T8sMLVqrjEHvZD2"};
+    public static final String[] ADMIN_UID = {"ImJjP2kUunTy9T8sMLVqrjEHvZD2", "gYDVw7KnheXoKU7X9IAbP9vl9Uf1"};
     public static final int GALLERY_INTENT = 1;
     public static final String ARG_ID = "ARG_ID";
     public static final String ARG_FRAGMENTO = "ARG_FRAGMENTO";
